41d2d1ac048d1cc6264d395e3865e6445b08ebda
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-12 02:44:24 +0200

    Add socials data: youtube, linkedin, github

---
+++ src/data/socials.js
@@ -0,0 +1,22 @@
+import {YouTube, GitHub, LinkedIn} from "./../ui/socialsComponents"
+
+export default [
+    {
+        svgComponent: GitHub,
+        color: "#000000",
+        url: "https://github.com/david-dkp",
+        socialName: "GitHub"
+    },
+    {
+        svgComponent: LinkedIn,
+        color: "#0A66C1",
+        url: "https://www.linkedin.com/in/david-dekeuwer-1940a01b9/",
+        socialName: "LinkedIn"
+    },
+    {
+        svgComponent: YouTube,
+        color: "#F50100",
+        url: "https://www.youtube.com/channel/UCcRAn1RjQvPQi2K2YFRYihw",
+        socialName: "YouTube"
+    },
+]
