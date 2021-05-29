d1f8e0a9b81a2c46d3abb8a13a0ae9ca2abc519d
David DEKEUWER <david.dekeuwer@gmail.com>
2021-05-29 02:55:10 +0200

    Add aos dependency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import {useEffect} from "react"
 import Navigation from "./ui/navigation/Navigation"
 import Home from "./ui/home/Home"
 import Apps from "./ui/apps/Apps"
 import Contact from "./ui/contact/Contact"
-
+import Aos from "aos"
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
 
 
 function App() {
+  useEffect(() => {
+    Aos.init()
+  }, [])
   return (
     <div className="App">
       <Router>
