48e14efc50a2df0cbb734e1a5d0f03326fbc8809
David DEKEUWER <david.dekeuwer@gmail.com>
2021-06-22 22:37:26 +0200

    Add 'See more' in RecentProjects

--- src/ui/home/RecentProjects.js
+++ src/ui/home/RecentProjects.js
@@ -3,15 +3,29 @@ import styled from "styled-components"
 import Header from "./Header"
 import AppProject from "./AppProject"
 import apps from "../../data/apps"
+import { useHistory } from "react-router-dom"
 
 function RecentProjects() {
+    const history = useHistory()
+
     return (
         <Container>
             <Header title="Mes projets récents" />
-
-            <div className="projects-container">
-                <AppProject app={apps[2]} />
-                <AppProject app={apps[3]} />
+            <div className="subcontainer">
+                <div className="projects-container">
+                    <AppProject app={apps[2]} />
+                    <AppProject app={apps[3]} />
+                </div>
+                <div
+                    className="see-more-container"
+                    onClick={() => history.push("/apps")}
+                >
+                    <div className="logo">
+                        <div className="first" />
+                        <div className="second" />
+                    </div>
+                    <div className="text">Voir plus</div>
+                </div>
             </div>
         </Container>
     )
@@ -22,15 +36,83 @@ const Container = styled.div`
     position: relative;
     display: flex;
     flex-direction: column;
-
     margin-top: 50px;
 
-    .projects-container {
+    .subcontainer {
+        justify-content: space-around;
+        align-items: center;
+        display: flex;
+        flex-wrap: wrap;
         margin-top: 70px;
+        gap: 70px;
+    }
+
+    .projects-container {
+        align-items: center;
         display: flex;
         justify-content: center;
         gap: 70px;
         flex-wrap: wrap;
+    }
+
+    .see-more-container {
+        width: 300px;
+        height: 300px;
+        border: 1px solid white;
+        box-shadow: 0px 0px 2px black;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-weight: lighter;
+        font-size: 3rem;
+        color: white;
+        transition: all 0.5s ease;
+
+        .logo {
+            position: relative;
+            width: 100px;
+            height: 100px;
+
+            * {
+                transition: all 0.5s ease;
+                position: absolute;
+            }
+
+            .first {
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                height: 6px;
+                width: 100px;
+                background-color: white;
+            }
+
+            .second {
+                left: 50%;
+                top: 50%;
+                transform: translate(-50%, -50%);
+                height: 100px;
+                width: 6px;
+                background-color: white;
+            }
+        }
+
+        .text {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+                Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+                sans-serif;
+        }
+
+    }
+
+    .see-more-container:hover {
+        background-color: white;
+        color: black;
+
+        .logo div {
+            background-color: black;
+        }
     }
 `
 
