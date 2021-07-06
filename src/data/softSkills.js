f65c975f68553841481453064272057296e97634
David DEKEUWER <david.dekeuwer@gmail.com>
2021-07-06 15:50:35 +0200

    Add softSkills data

---
+++ src/data/softSkills.js
@@ -0,0 +1,34 @@
+import {
+    AutonomousLogo,
+    CuriousLogo,
+    AmbitiousLogo,
+    ListeningLogo,
+} from "../ui/common/svgs"
+
+const softSkills = [
+    {
+        title: "Autonome",
+        description: "J’aime chercher les informations la ou c’est censé être.",
+        logo: AutonomousLogo,
+    },
+    {
+        title: "Curieux",
+        description:
+            "Découvrir les nouvelles technologies est ma seconde nature",
+        logo: CuriousLogo,
+    },
+    {
+        title: "Ambitieux",
+        description:
+            "J’ai un regard ambitieux sur tout les projets que j’entreprends.",
+        logo: AmbitiousLogo,
+    },
+    {
+        title: "À l'écoute",
+        description:
+            "J’écoute votre projet, vos objectifs, mes collègues, mon équipe en sah.",
+        logo: ListeningLogo,
+    },
+]
+
+export default softSkills
