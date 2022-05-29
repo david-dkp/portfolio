dba46c61a110107d614c516e84d5280c667d59d3
David <david.dekeuwer@gmail.com>
2022-05-29 14:55:36 +0200

    fix: overlapping Footer text on mobile

--- src/ui/home/footer/Footer.js
+++ src/ui/home/footer/Footer.js
@@ -13,11 +13,10 @@ function Footer() {
 const Container = styled.section`
   background-color: #1f283d;
   color: white;
-  font-size: 22px;
+  font-size: clamp(12px, 5vw, 22px);
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  line-height: 0;
-  padding: 3em 2em;
+  padding: 2.5em 2em;
 
   span {
     color: grey;
